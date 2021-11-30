baac68f2fa96160413b3be02d6e62c2869ab649d
AnuragKumar <anuragkumarrr@gmail.com>
2021-11-29 21:59:08 -0600

    Topic - Events

--- mysecondreactapp/src/component/Header.js
+++ mysecondreactapp/src/component/Header.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 
 class Header extends React.Component{
-    constructor(){
-        super()
+  
+  constructor(){
+      super();
 
-        this.state ={
-            title:'React App',
-            keyword :'Enter the user input'
-        }
+      this.state = {
+          title: 'React Application',
+          keyword:'User input here'
+      }
+  }
 
-    }
-
-   handleChange = (event) => {
-       console.log(event.target.value)
-       this.setState({
-           keyword:event.target.value ? event.target.value : 'Enter the user input'
-        })
-   }
-
+  handleChange = (event) => {
+    // console.log(event);
+    // console.log(event.target.value);
+    this.setState({keyword:event.target.value ? event.target.value : 'User input here' });
+  }
     render(){
         return (
             <div>
-                    <h1>Welcome to {this.state.title}</h1>
-                    <input onChange={this.handleChange}/>
-                    <div> {this.state.keyword}</div>
+                   <center><h1>Welcome to {this.state.title}</h1>
+                        <input type="text" onChange={this.handleChange}/>
+                        <h2>{this.state.keyword}</h2>
+                   </center>
+                   <hr/>
             </div>
         )
     }
